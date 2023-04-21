3ab08a266125351e4f85214fa18e320620516fb2
Nathan <omarnate1@gmail.com>
2023-04-21 12:57:19 +0100

    my web dev

---
+++ index.js
@@ -0,0 +1,19 @@
+const navlink = document.querySelector(".navlinks");
+const body = document.querySelector("body");
+const section2cardContainer = document.querySelector(".section2cardContainer");
+const handleShowSidebar = () => {
+  navlink.classList.add("navlinksShown");
+  body.classList.add("bodyUnscroll");
+};
+const handleRemoveSidebar = () => {
+  navlink.classList.remove("navlinksShown");
+  body.classList.remove("bodyUnscroll");
+};
+
+const handleScrollRight = () => {
+  section2cardContainer.scrollBy(500, 0);
+};
+
+const handleScrollLeft = () => {
+  section2cardContainer.scrollBy(-500, 0);
+};
